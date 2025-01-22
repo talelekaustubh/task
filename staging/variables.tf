8b51507cfe244222442f7cdc7fbfc06ab50db7e6
unknown <kaustubh.talele2018@gmail.com>
2025-01-22 22:55:22 +0530

    Updating Staging

---
+++ staging/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+}
